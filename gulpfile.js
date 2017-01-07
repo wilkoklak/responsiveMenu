f3c9dd006d23b8da9ea42810261a5f2f68d6e1d7
Artur <thereaperv11@gmail.com>
2017-01-07 16:08:55 +0100

    Working version

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 const gulp = require('gulp');
 const compass = require('gulp-compass');
-const uglify = require('gulp-uglify');
+const CleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const watch = require('gulp-watch');
 
-gulp.task('default', () => {
+gulp.task('build', () => {
 	gulp.src('src/*.sass')
 		.pipe(compass({
 			css: 'dist/',
 			sass: 'src',
-			task: 'watch'
+			require: 'sass-globbing'
 		}))
 		.pipe(gulp.dest('dist/'))
 })
+gulp.task('minify', () => {
+	gulp.src('dist/*.css')
+		.pipe(CleanCSS())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/minified'))
+})
+
+gulp.task('default', () => {
+	gulp.watch('src/**/*.sass', ['build'])
+	gulp.watch('dist/*.css', ['minify'])
+});
